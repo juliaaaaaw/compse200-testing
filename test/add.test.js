c67830dd9a377c0f2d0e7feb78fc869847b4b696
Jaakko Utriainen <jaakko.utriainen@tuni.fi>
2025-12-11 20:03:02 +0200

    remove mock test files

--- test/add.test.js
+++
@@ -1,9 +0,0 @@
-// test/add.test.js
-import { add } from '../src/index.js';
-import { expect } from 'chai';
-
-describe("add", () => {
-  it("adds numbers", () => {
-    expect(add(2, 3)).to.equal(5);
-  });
-});
